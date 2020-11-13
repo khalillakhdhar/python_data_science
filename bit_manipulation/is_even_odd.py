7c1ad24f189d5c68ea0df553aaa37031804d3e73
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-13 08:36:17 +0100

    Add files via upload

---
+++ bit_manipulation/is_even_odd.py
@@ -0,0 +1,58 @@
+# YouTube Video: https://www.youtube.com/watch?v=F0RywIXmqlU&index=2&list=PL5tcWHG-UPH1u5iox6v1Hey59vNd5cnTw
+"""
+Write a program to determine if a given number is even or odd.
+Do not make use of the modulus operator.
+"""
+
+
+"""
+  : 4 2 1
+0 : 0 0 0
+1 : 0 0 1
+2 : 0 1 0
+3 : 0 1 1
+4 : 1 0 0
+5 : 1 0 1
+6 : 1 1 0
+7 : 1 1 1
+
+Note:
+    Adding one to an even number makes it odd.
+    Adding any number of even numbers will result in an even number.
+"""
+
+"""
+Example:
+    1 : 0 0 1
+        0 0 1
+        -----
+        0 0 1
+
+    3 : 0 1 1
+        0 0 1
+        -----
+        0 0 1
+
+    6 : 1 1 0
+        0 0 1
+        0 0 0
+
+Observation:
+    ANDing the number with 1 gives either "0" or "1":
+        0 : If even.
+        1 : If odd.
+"""
+
+
+def is_even_odd(x: int):
+    if x & 1 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+# Yields "Even":
+print(is_even_odd(26))
+
+# Yields "Odd":
+print(is_even_odd(25))
---
+++ bit_manipulation/set_nth_bit.py
@@ -0,0 +1,44 @@
+# YouTube Video: https://www.youtube.com/watch?v=ZfU00JFhL7E&index=4&list=PL5tcWHG-UPH1u5iox6v1Hey59vNd5cnTw
+"""
+Write a program  that takes an integer and
+sets the n-th bit in the binary representation of
+that integer
+
+For instance, the binary representation of 6 is:
+    110
+
+The least significant bit is the bit on the far right
+of the binary representation and the most significant
+bit is the bit on the far left. We order the bits as
+
+b2, b1, b0
+1   1   0
+
+For our function, if we set the 0th bit, we should obtain
+the binary representation:
+    1 1 1
+"""
+
+
+"""
+We can combine the shift operator with the same idea used in the video
+to determine if the nth bit is set:
+    XXX
+
+Example: Set 0th bit:
+
+    6       : 1 1 0
+    1 << 0  : 0 0 1
+            -------- |
+              1 1 1
+"""
+
+
+def set_nth_bit(x: int, n: int):
+    # If you just want to return the binary
+    # representation of the number:
+    #return bin(x | 1 << n)[2:]
+    return x | 1 << n
+
+
+print(set_nth_bit(6, 0))
